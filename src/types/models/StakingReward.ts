c2391b2f1a82065549ee5eb4352b579277f98f2f
Mykola Bezruk <mykola.bezruk@clearmove.com>
2022-01-09 13:58:55 +0200

    sub-2

--- src/types/models/StakingReward.ts
+++ src/types/models/StakingReward.ts
@@ -1,11 +1,16 @@
 // Auto-generated , DO NOT EDIT
-import { Entity, FunctionPropertyNames } from '@subql/types';
+import {Entity, FunctionPropertyNames} from "@subql/types";
 import assert from 'assert';
 
+
+
+
 export class StakingReward implements Entity {
+
     constructor(id: string) {
         this.id = id;
     }
+
 
     public id: string;
 
@@ -17,30 +22,33 @@ export class StakingReward implements Entity {
 
     public blockHeight: number;
 
-    async save(): Promise<void> {
+
+    async save(): Promise<void>{
         let id = this.id;
-        assert(id !== null, 'Cannot save StakingReward entity without an ID');
+        assert(id !== null, "Cannot save StakingReward entity without an ID");
         await store.set('StakingReward', id.toString(), this);
     }
-    static async remove(id: string): Promise<void> {
-        assert(id !== null, 'Cannot remove StakingReward entity without an ID');
+    static async remove(id:string): Promise<void>{
+        assert(id !== null, "Cannot remove StakingReward entity without an ID");
         await store.remove('StakingReward', id.toString());
     }
 
-    static async get(id: string): Promise<StakingReward | undefined> {
-        assert(id !== null && id !== undefined, 'Cannot get StakingReward entity without an ID');
+    static async get(id:string): Promise<StakingReward | undefined>{
+        assert((id !== null && id !== undefined), "Cannot get StakingReward entity without an ID");
         const record = await store.get('StakingReward', id.toString());
-        if (record) {
+        if (record){
             return StakingReward.create(record);
-        } else {
+        }else{
             return;
         }
     }
 
+
+
     static create(record: Partial<Omit<StakingReward, FunctionPropertyNames<StakingReward>>> & Entity): StakingReward {
-        assert(typeof record.id === 'string', 'id must be provided');
+        assert(typeof record.id === 'string', "id must be provided");
         let entity = new StakingReward(record.id);
-        Object.assign(entity, record);
+        Object.assign(entity,record);
         return entity;
     }
 }
